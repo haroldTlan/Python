d60aadf41e80d8ffccfc3f8771e58152a45f1a8e
haroldTlan <369847634@qq.com>
2016-08-04 09:46:46 +0800

    all use peewee

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Python
 sth about Python
+
+
+
+Peewee
+
+使用python把原本是shell写的恢复脚本转换过来，
+直接操作b数据库，而不是使用os.system机械的使用命令。
+其中就涉及到了peewee这个模块，对于使用sql足够了，
+在空间足够的情况下还是使用mongodb好。
--- peewee/recovery.py
+++ peewee/recovery.py
@@ -3,17 +3,17 @@
 import sys
 import os
 from util import *
+import db
 import adm
 import time
 
 #To get the failed raids
 def get_failed_raids():
-    _, results = execute("echo 'use speediodb; select * from raids;' | mysql -uroot -ppasswd | grep failed")
     raids = []
-    for result in results.rstrip().split('\n'):
-        o = result.split('\t')
-        raids.append({'uuid': o[0], 'dev_name': o[12], 'name': o[3]})
-        
+    results = adm.Raid.all()
+    for i in results:
+        if i.health == 'failed':
+            raids.append(i)     
     return raids
 
 #To judge if all the disks in the raid with uuid 'raid_uuid' are plugged
@@ -21,12 +21,17 @@ def get_failed_raids():
 #raid_uuid is the uuid of the failed raid
 def check_disk_nr(recovery_logs, raid_uuid):
     recs_about_raid = [rec for rec in recovery_logs if rec['raid_uuid'] == raid_uuid]
-    
-    _, results = execute("echo 'use speediodb; select * from disks;' | mysql -uroot -ppasswd |grep %s" % raid_uuid)
 
     disks_in_db = {}
-    for result in results.rstrip().split('\n'):
-        disks_in_db[result.split('\t')[0]] = result.split('\t')[17] 
+    failed_disks = []
+    disks = adm.Disk.all()
+    for i in disks:
+        if i.raid:
+            if i.raid.uuid == raid_uuid:
+                failed_disks.append(i)
+
+    for disk in failed_disks:
+        disks_in_db[disk.uuid] = disk.dev_name
 
     _, disks_exist_now = execute('ls /dev/sd[a-z]')
     for disk_uuid in recs_about_raid[-1]['data_disks']:
@@ -37,21 +42,38 @@ def check_disk_nr(recovery_logs, raid_uuid):
 
     return True
 
+def unicode_str(uni):
+    if isinstance(uni, list):
+        return [str(i) for i in uni]
+    if isinstance(uni, unicode):
+        return str(uni)
+
 #To find the latest recovery log about the raid with the raid_uuid input
-def check_raid_recovery_log():
-    _, results = execute("mysqldump -uroot -ppasswd speediodb | grep 'INSERT INTO' | grep raid_recoveries | sed 's/),(/)\t(/g'")
+def check_raid_recovery_log():  
+    recs = []
+    results = db.RaidRecovery().all()
+#    results = adm.Raid.all()
     if results == '':
         return False
-
-    recs = []
-    for result in results.rstrip().split('\t'):
-        obs = re.search(r'\(([^\(\)]+)\)', result).group(1).split(',')
-        o = [ob.strip('[\']') for ob in obs]
-        ddisk = [o[8+i] for i in range(0, int(o[7]))]
-        sdisk = o[-1]
-        recs.append({'raid_uuid': o[3], 'raid_name': o[4], 'data_disks': ddisk, 'spare_disks': sdisk, 'time': o[1]})
+    
+    for result in results:
+        r_uuid = unicode_str(result.uuid)
+        r_name = unicode_str(result.name)
+        d_disks = unicode_str(result.raid_disks.split(','))
+        s_disks = unicode_str(result.spare_disks)
+        time = result.created_at.strftime("%Y-%m-%d %H:%M:%S")
+        recs.append({'raid_uuid': r_uuid, 'raid_name': r_name, 'data_disks': d_disks, 'spare_disks': s_disks, 'time': time})
 
     return recs
+#    for result in results:
+#        if result.health == 'failed':
+#            d_disks = [i.uuid for i in result.disks]
+#            s_disks = [i.uuid for i in result.spare_disks]
+#            r_uuid = result.uuid
+#            r_name = result.name
+#            recs.append({'raid_uuid': r_uuid, 'raid_name': r_name, 'data_disks': d_disks, 'spare_disks': s_disks})
+
+#    return recs
 
 #According to the raid_rec input to change information in database
 def add_disk(recovery_logs, raid_uuid):
@@ -76,13 +98,13 @@ def touch_file():
 if __name__ == "__main__":
     failed_raids = get_failed_raids()
     recs = check_raid_recovery_log()
-    if check_disk_nr(recs, failed_raids[0]['uuid']):
+    if check_disk_nr(recs, failed_raids[0].uuid):
         print 'OK! all the disk in the failed raid are plugged'
     else:
-        print 'Please plug all the disk of the failed raid %s. And try again.' % failed_raids[0]['name']
+        print 'Please plug all the disk of the failed raid %s. And try again.' % failed_raids[0].name
         exit(1)
 
-    if add_disk(recs, failed_raids[0]['uuid']):
+    if add_disk(recs, failed_raids[0].uuid):
         print 'Added disks success!'
     else:
         print 'Added disks failed!'
